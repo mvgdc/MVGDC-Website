b241acfa1eab5107b899221220c9f0e89a84998a
bhavan060 <70976789+bhavan060@users.noreply.github.com>
2022-09-16 17:14:35 -0700

    made private false

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
 
     <Router className="main">
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" exact element={<Home />} />
         <Route path="/team" element={<Team />} />
         <Route path="/resources" element={<Resources />} />
       </Routes>
