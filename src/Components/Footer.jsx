288caf2a5bcd89661d90b6da170f214e3caed775
bhavan060 <70976789+bhavan060@users.noreply.github.com>
2023-09-25 16:57:01 -0700

    fixed a bunch of stuff

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -3,14 +3,13 @@ import React from "react";
 function Footer(){
     return(
         <>
-            <div style={{ display: "flex", flexDirection: "row", color: "rgb(109, 107, 107)", marginTop: "40px" }}>
+            <div className="footer-temp" style={{ display: "flex", flexDirection: "row", color: "rgb(109, 107, 107)", marginTop: "40px" }}>
                 <div style={{ width: "50%", paddingLeft: "10px" }}>
                 &copy; 2022 MVGDC
                 </div>
                 <div style={{ width: "50%", textAlign: "right", paddingRight: "10px" }}>
                 Designed by Babun
                 </div>
-
             </div>
         </>
     );
